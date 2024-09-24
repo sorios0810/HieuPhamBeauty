aac21c6bbe5f45cad1a2cb547e724c45146288dd
sorios0810 <hunganhnguyen2001@gmail.com>
2024-09-25 01:06:16 +0700

    Fix layout

--- HieuPhamBeauty/HieuPhamBeauty/App_Start/BundleConfig.cs
+++ HieuPhamBeauty/HieuPhamBeauty/App_Start/BundleConfig.cs
@@ -31,6 +31,36 @@ namespace HieuPhamBeauty
                       "~/Content/css/landingpage/owl.carousel.min.css",
                       "~/Content/css/landingpage/style.css",
                       "~/Content/css/landingpage/responsive.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/lib").Include(
+                     "~/Content/css/lib/bootstrap.min.css",
+                     "~/Content/css/lib/ionicons.min.css",
+                     "~/Content/css/lib/simple-line-icons.css",
+                     "~/Content/css/lib/lineIcons.css",
+                     "~/Content/css/lib/font-awesome.min.css",
+                     "~/Content/css/lib/animate.css",
+                     "~/Content/css/lib/swiper.min.css",
+                     "~/Content/css/lib/range-slider.css",
+                     "~/Content/css/lib/fancybox.min.css",
+                     "~/Content/css/lib/slicknav.css",
+                     "~/Content/css/lib/owlcarousel.min.css",
+                     "~/Content/css/lib/owltheme.min.css",
+                     "~/Content/css/lib/spacing.css"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/lib").Include(
+                      "~/Scripts/lib/modernizr.js",
+                      "~/Scripts/lib/jquery-main.js",
+                      "~/Scripts/lib/jquery-migrate.js",
+                      "~/Scripts/lib/bootstrap.min.js",
+                      "~/Scripts/lib/jquery.appear.js",
+                      "~/Scripts/lib/swiper.min.js",
+                      "~/Scripts/lib/fancybox.min.js",
+                      "~/Scripts/lib/slicknav.js",
+                      "~/Scripts/lib/waypoints.js",
+                      "~/Scripts/lib/owlcarousel.min.js",
+                      "~/Scripts/lib/jquery-match-height.min.js",
+                      "~/Scripts/lib/jquery-zoom.min.js",
+                      "~/Scripts/lib/countdown.js"));
         }
     }
 }
---
+++ HieuPhamBeauty/HieuPhamBeauty/Scripts/Views/Home.js
@@ -0,0 +1,117 @@
+﻿const data = [
+    {
+        ID: 1,
+        IMG_1: "../../Content/img/shop/01.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 1
+    },
+    {
+        ID: 2,
+        IMG_1: "../../Content/img/shop/01.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 1
+    },
+    {
+        ID: 3,
+        IMG_1: "../../Content/img/shop/01.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 1
+    },
+    {
+        ID: 4,
+        IMG_1: "../../Content/img/shop/01.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 1
+    },
+    {
+        ID: 5,
+        IMG_1: "../../Content/img2/sample.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 2
+    },
+    {
+        ID: 6,
+        IMG_1: "../../Content/img2/sample.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 2
+    },
+    {
+        ID: 7,
+        IMG_1: "../../Content/img2/sample.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 2
+    },
+    {
+        ID: 8,
+        IMG_1: "../../Content/img2/sample.jpg",
+        IMG_2: "../../Content/img/shop/01-h1.jpg",
+        TITLE: "Nâng cơ xóa nhăn mắt",
+        SLIDE_TYPE: 2
+    },
+];
+
+function ShowPopup() {
+    var popupProduct = $(".popup-product-quickview");
+
+    popupProduct.addClass('active');
+    $(".popup-product-overlay").addClass('active');
+    $(".popup-product-close").addClass('active');
+    $("body").addClass("fix");
+}
+
+function RenderBoxList(data) {
+    var itemHTML_1 = "", itemHTML_2 = "";
+
+    data.forEach((obj) => {
+        if (obj.SLIDE_TYPE == 1) {
+            itemHTML_1 += `
+                <div class="item type1">
+                    <div class="product-item" onclick="ShowPopup()">
+                        <div class="inner-content">
+                            <div class="product-thumb">
+                               <img src="${obj.IMG_1}" alt="${obj.TITLE}">
+                               <img class="second-image" src="${obj.IMG_2}" alt="${obj.TITLE}">
+                            </div>
+                            <div class="product-desc">
+                                <div class="product-info">
+                                    <div class="horizontal-line"></div>
+                                    <h4 class="title">${obj.TITLE}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+        else if (obj.SLIDE_TYPE == 2) {
+            itemHTML_2 += `
+                <div class="item type2">
+                    <div class="product-item" onclick="ShowPopup()">
+                        <div class="inner-content">
+                            <div class="product-thumb">
+                               <img src="${obj.IMG_1}" alt="${obj.TITLE}">
+                               <img class="second-image" src="${obj.IMG_2}" alt="${obj.TITLE}">
+                            </div>
+                            <div class="product-desc">
+                                <div class="product-info">
+                                    <div class="horizontal-line"></div>
+                                    <h4 class="title">${obj.TITLE}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    });
+
+    $("#listBox1").html(itemHTML_1);
+    $("#listBox2").html(itemHTML_2);
+}
